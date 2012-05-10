17f2b70fb24b728db1f6d2ef1413671f0a9fd4bc
mmohamed <mohamedelshami20@gmail.com>
2012-05-10 10:52:50 +0300

    First Commit :).

---
+++ SawatAndroidClient/src/com/shami/devel/SawatAndroidClientActivity.java
@@ -0,0 +1,13 @@
+package com.shami.devel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SawatAndroidClientActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
